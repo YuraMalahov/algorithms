994082bdc6c49b5bf9c9da5f6127ccd38f38a88e
yura <yura.malahov@gmail.com>
2015-11-09 00:06:13 +0200

    added merge

---
+++ sorts/js/merge.js
@@ -0,0 +1,47 @@
+"use strict";
+
+var a = [2,3,1,5,9,4,0,8,6,7];
+
+var checkCount = 0,
+    replaceCount = 0;
+
+function sort(array, lowest, higest) {
+    if (higest <= lowest) {
+        return;
+    }
+
+    let mid = lowest + Math.floor((higest - lowest) / 2);
+
+    sort(array, lowest, mid);
+    sort(array, mid + 1, higest);
+    merge(array, lowest, mid, higest);
+}
+
+function merge(array, lowest, middle, highest) {
+    let i = lowest,
+        j = middle + 1,
+        aux = [];
+
+    for (let k = lowest; k <= highest; k++) {
+        aux[k] = array[k];
+    }
+
+    for (let k = lowest; k <= highest; k++) {
+        if (i > middle) {
+            array[k] = aux[j++];
+        } else if (j > highest) {
+            array[k] = aux[i++];
+        } else if (aux[j] < aux[i]) {
+            array[k] = aux[j++];
+        } else {
+            array[k] = aux[i++];
+        }
+    }
+}
+
+console.log(a);
+sort(a, 0, a.length - 1);
+console.log(a);
+
+console.log('checks', checkCount);
+console.log('replaces', replaceCount);
