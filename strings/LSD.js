fcdb184b85247c7fe81f739808f4eef23206cf17
Yurij <yura.malahov@gmail.com>
2015-12-19 22:58:57 +0200

    Create LSD.js

---
+++ strings/LSD.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var a = [
+    'AGXHK',
+    '39X7F',
+    'NCQ03',
+    'NB67R',
+    'DUMRN',
+    '5UATT',
+    'OXIX5',
+    'NAZ00',
+    '27QEW',
+    'HTIGQ'
+],
+    aux = [],
+    R = 256,
+    W = 5,
+    N = a.length;
+
+for (let d = W - 1; d >= 0; d--) {
+    let count = new Array(R).fill(0, 0, 256);
+
+    for (let i = 0; i < N; i++) {
+        count[a[i].charCodeAt(d) + 1]++;
+    }
+
+    for (let r = 0; r < R; r++) {
+        count[r + 1] += count[r];
+    }
+
+    for (let i = 0; i < N; i++) {
+        aux[count[a[i].charCodeAt(d)]++] = a[i];
+    }
+
+    a = aux.slice();
+}
+
+console.log(a);
+
