b7be53e6a2b9d19d2d1737485cfb5efa99a955fe
Yurij <yura.malahov@gmail.com>
2016-06-05 08:18:21 +0300

    Update binary.js

--- search/binary.js
+++ search/binary.js
@@ -1,25 +1,28 @@
 "use strict";
 
-function search(input, n) {
+function search(input, searched) {
     let start = 0,
-        end = input.length,
+        end = input.length - 1,
         mid = Math.floor(end / 2);
 
-    while (start !== end && start < end) {
-        let num = input[mid];
-
-        if (n === num) {
-            return true;
-        } else if (n < num) {
-            end = mid;
-            mid = Math.floor((mid + start) / 2);
-        } else if (n > num) {
-            start = mid;
-            mid = Math.floor((mid + end) / 2);
-        }
+    if (searched < input[start] || searched > input[end]) {
+        return -1;
     }
 
-    return false;
+    while (start <= end) {
+        let number = input[mid];
+
+        if (searched === number) {
+            return mid;
+        } else if (searched < number) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+        mid = Math.floor((end + start) / 2);
+    }
+
+    return -1;
 }
 
-console.log(search([0,1,2,3,4,5,6,7,8,9], 9));
+console.log(search([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 20));
