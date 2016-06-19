0f112f35f2e2e72f3a23eae73a83952ae9174e81
Yuri <yura.malahov@gmail.com>
2016-06-19 13:13:05 +0300

    Update bag.js

--- datastructures/js/bag.js
+++ datastructures/js/bag.js
@@ -1,50 +1,32 @@
 "use strict";
 
-const node = require('./node');
+const node = require("./node"),
+    NodeIterator = require("./node-iterator");
+
+class Bag extends NodeIterator {
+    constructor() {
+        super();
+        this._size = 0;
+    }
+
+    /**
+     * @param {*} value
+     */
+    add(value) {
+        let oldFirst = this._first;
+        this._first = node.createNode(value);
+        this._first.next = oldFirst;
+        this._size++;
+    }
+
+    /**
+     * @returns {number}
+     */
+    size() {
+        return this._size;
+    }
+}
 
 module.exports.createBag = function () {
-    /**
-     * @type {null|Node}
-     */
-    let first = null;
-    
-    class Bag {
-        /**
-         * @param {*} value
-         */
-        add(value) {
-            let oldFirst = first;
-
-            first = node.createNode(value);
-            first.next = oldFirst;
-        }
-    
-        /**
-         * @returns {Object}
-         */
-        [Symbol.iterator]() {
-            let current = first;
-    
-            return {
-                next() {
-                    if (current) {
-                        let value = current.value;
-                        
-                        current = current.next;
-                        
-                        return {
-                            value: value,
-                            done: false
-                        };
-                    }
-    
-                    return {
-                        done: true
-                    };
-                }
-            }
-        }
-    }
-    
     return new Bag();
-}; 
+};
