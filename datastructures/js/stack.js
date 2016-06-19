dee7810d2e9c94796479ffa0e8c28e258227cf50
Yuri <yura.malahov@gmail.com>
2016-06-19 13:12:48 +0300

    Update stack.js

--- datastructures/js/stack.js
+++ datastructures/js/stack.js
@@ -1,45 +1,45 @@
 "use strict";
 
-const node = require("./node");
+const node = require("./node"),
+    NodeIterator = require("./node-iterator");
+
+class Stack extends NodeIterator {
+    constructor() {
+        super();
+    }
+
+    /**
+     * @param {*} item
+     */
+    push(item) {
+        let oldFirst = this._first;
+
+        this._first = node.createNode(item);
+        this._first.next = oldFirst;
+    }
+
+    /**
+     * @returns {null|Node}
+     */
+    pop() {
+        let oldFirst = this._first;
+
+        if (this.isEmpty()) {
+            return oldFirst;
+        }
+        this._first = this._first.next;
+
+        return oldFirst.value;
+    }
+
+    /**
+     * @returns {boolean}
+     */
+    isEmpty() {
+        return this._first === null;
+    }
+}
 
 module.exports.createStack = function () {
-    /**
-     * @type {null|Node}
-     */
-    let first = null;
-
-    class Stack {
-        /**
-         * @param {*} item
-         */
-        push(item) {
-            let oldFirst = first;
-
-            first = node.createNode(item);
-            first.next = oldFirst;
-        };
-
-        /**
-         * @returns {null|Node}
-         */
-        pop() {
-            let oldFirst = first;
-
-            if (this.isEmpty()) {
-                return oldFirst;
-            }
-            first = first.next;
-
-            return oldFirst.value;
-        };
-
-        /**
-         * @returns {boolean}
-         */
-        isEmpty() {
-            return first === null;
-        };
-    }
-    
     return new Stack ();
 };
