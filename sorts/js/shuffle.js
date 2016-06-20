cdad6f81f9f56ed654d70a04022c7e778eeeec43
YuraMalahov <yura.malahov@gmail.com>
2016-06-20 18:36:30 +0300

    added shuffle

--- sorts/js/shuffle.js
+++ sorts/js/shuffle.js
@@ -1,3 +1,15 @@
-/**
- * Created by yura on 20.06.16.
- */
+"use strict";
+
+function shuffle(array) {
+    for (let i = 0, count = array.length; i < count; i++) {
+        let rand = Math.round(Math.random() * (count-1));
+
+        [array[rand], array[i]] = [array[i], array[rand]];
+    }
+}
+
+var a = [0,1,2,3,4,5,6,7,8,9];
+
+shuffle(a);
+
+console.log(a);
