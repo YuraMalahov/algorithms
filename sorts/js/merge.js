26efb1e43b805f941be3c3dc4b76112b6ac62db4
YuraMalahov <yura.malahov@gmail.com>
2016-06-24 22:06:59 +0300

    fix typo

--- sorts/js/merge.js
+++ sorts/js/merge.js
@@ -1,40 +1,46 @@
 "use strict";
 
-var a = [2,3,1,5,9,4,0,8,6,7];
 
-function sort(array, lowest, higest) {
-    if (higest <= lowest) {
-        return;
+function sort (array) {
+    let aux = [];
+
+    sort(array, aux, 0, a.length - 1);
+
+    function sort(array, aux, lowest, highest) {
+        if (highest <= lowest) {
+            return;
+        }
+
+        let mid = lowest + Math.floor((highest - lowest) / 2);
+
+        sort(array, aux, lowest, mid);
+        sort(array, aux, mid + 1, highest);
+        merge(array, aux, lowest, mid, highest);
     }
 
-    let mid = lowest + Math.floor((higest - lowest) / 2);
+    function merge(array, aux, lowest, middle, highest) {
+        let i = lowest,
+            j = middle + 1;
 
-    sort(array, lowest, mid);
-    sort(array, mid + 1, higest);
-    merge(array, lowest, mid, higest);
-}
+        for (let k = lowest; k <= highest; k++) {
+            aux[k] = array[k];
+        }
 
-function merge(array, lowest, middle, highest) {
-    let i = lowest,
-        j = middle + 1,
-        aux = [];
-
-    for (let k = lowest; k <= highest; k++) {
-        aux[k] = array[k];
-    }
-
-    for (let k = lowest; k <= highest; k++) {
-        if (i > middle) {
-            array[k] = aux[j++];
-        } else if (j > highest) {
-            array[k] = aux[i++];
-        } else if (aux[j] < aux[i]) {
-            array[k] = aux[j++];
-        } else {
-            array[k] = aux[i++];
+        for (let k = lowest; k <= highest; k++) {
+            if (i > middle) {
+                array[k] = aux[j++];
+            } else if (j > highest) {
+                array[k] = aux[i++];
+            } else if (aux[j] < aux[i]) {
+                array[k] = aux[j++];
+            } else {
+                array[k] = aux[i++];
+            }
         }
     }
 }
 
-sort(a, 0, a.length - 1);
+var a = [2,3,1,5,9,4,0,8,6,7];
+
+sort(a);
 console.log(a);
