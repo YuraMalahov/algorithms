d29c7309397b952f8e73fd205dc53a2da59cc286
YuraMalahov <yura.malahov@gmail.com>
2016-07-21 22:05:20 +0300

    patch apply

--- test.js
+++ test.js
@@ -7,28 +7,36 @@ var fs = require("fs");
 var file = fs.createWriteStream("./text_file");
 var child = childProcess.fork("./server.js", [], {});
 
-child.on("message", function (message) {
-    if (message === "OK") {
-        childProcess.exec("npm install", function (error, stdout, stderr) {
-            if (error) {
-                console.error(`exec error: ${error}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
-            
-            var req = http.request({
-                hostname: '127.0.0.1',
-                port: 8000,
-                path: '/',
-                method: 'GET'
-            }, function (res) {
-                res.pipe(file);
-                res.on('end', () => console.log('No more data in response.'));
+childProcess.exec("git apply message.patch", function () {
+    child.on("message", function (message) {
+        if (message === "OK") {
+            childProcess.exec("npm install", function (error, stdout, stderr) {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+                console.log(`stderr: ${stderr}`);
+                
+                var req = http.request({
+                    hostname: '127.0.0.1',
+                    port: 8000,
+                    path: '/',
+                    method: 'GET'
+                }, function (res) {
+                    res.pipe(file);
+                    res.on('end', function () {
+                        console.log('No more data in response.');
+                        childProcess.exec("git reset --hard HEAD", function () {
+                            child.kill();
+                            process.exit();
+                        });
+                    });
+                });
+                req.on('error', (e) => console.log(`problem with request: ${e.message}`));
+                req.write('');
+                req.end();
             });
-            req.on('error', (e) => console.log(`problem with request: ${e.message}`));
-            req.write('');
-            req.end();
-        });
-    }
+        }
+    });
 });
