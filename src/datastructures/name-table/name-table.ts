e151cc51a7c0d5998bb41e91004e3ba6c40e8911
Iurii Malakhovskyi <iurii.malakhovskyi@refinitiv.com>
2020-08-03 21:48:25 +0300

    add implementation for name-table put & get methods

--- src/datastructures/name-table/name-table.ts
+++ src/datastructures/name-table/name-table.ts
@@ -1,11 +1,34 @@
+import { HashCode } from "./hash-code";
 
 class NameTable {
-    put(key: string, val: any) {
+    private hashCode: HashCode;
+    private keys: Array<string>;
+    private values: Array<any>;
+    private tableSize: number;
 
+    constructor(tableSize: number) {
+        this.tableSize = tableSize;
+        this.hashCode = new HashCode();
+        this.keys = new Array();
+        this.values = new Array();
     }
 
-    get(key: string) {
+    put(key: string, val: any) {
+        // converts hashcode into positive and takes mode of table size
+        const hash = this.hashCode.hashCode(key) & 0x7fffffff % this.tableSize;
 
+        this.keys[hash] = key;
+        this.values[hash] = val;
+    }
+
+    get(key: string): any {
+        const hash = this.hashCode.hashCode(key) & 0x7fffffff % this.tableSize;
+
+        if (this.keys[hash] !== key) {
+            return null;
+        }
+
+        return this.values[hash];
     }
 
     find(key: string) {
