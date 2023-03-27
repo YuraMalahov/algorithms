57d0dca72749598392fc633d36347e9eb4de7002
Iurii Malakhovskyi <iurii.malakhovskyi@lseg.com>
2023-03-27 20:25:25 +0300

    add second solution for finding cicles in the linked list

--- src/quizzes/linked-list-cicle.ts
+++ src/quizzes/linked-list-cicle.ts
@@ -25,3 +25,25 @@ function hasCycle(head: ListNode | null): boolean {
 
   return false;
 };
+
+function hasCycle2(head: ListNode | null): boolean {
+  let current = head?.next;
+  let tail = head;
+  let i = 1;
+
+  while (current) {
+    if (tail === current) {
+      return true;
+    }
+
+    current = current.next;
+
+    if (i % 2 === 0) {
+      tail = tail?.next;
+    }
+
+    i++;
+  }
+
+  return false;
+};
